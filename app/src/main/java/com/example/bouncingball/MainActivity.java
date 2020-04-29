f578e83f512431de8b68b218606731239d56e366
gonpuga <gonpuga@gmail.com>
2020-04-29 12:34:17 +0200

    Bouncing Ball with SurfaceView

--- app/src/main/java/com/example/bouncingball/MainActivity.java
+++ app/src/main/java/com/example/bouncingball/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.bouncingball;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new BouncingBallView(this));
     }
